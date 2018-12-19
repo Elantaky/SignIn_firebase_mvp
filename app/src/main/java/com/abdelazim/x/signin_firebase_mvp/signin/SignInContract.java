3e8626060baab4df415e334d4d5c661ecad9773b
Abdelaziz <Abdelaziz.elantaky@Gmail.com>
2018-12-19 19:46:09 +0200

    Initial commit (THIS CODE HAS BEEN MADE BY MOHAMED ABDELAZEEM)

---
+++ app/src/main/java/com/abdelazim/x/signin_firebase_mvp/signin/SignInContract.java
@@ -0,0 +1,64 @@
+package com.abdelazim.x.signin_firebase_mvp.signin;
+
+import android.content.DialogInterface;
+
+public interface SignInContract {
+
+    interface presenter {
+
+        void signInClicked();
+
+        void registerClicked();
+
+        void dialogSignInClicked(DialogInterface dialog, String email, String password);
+
+        void dialogRegisterClicked(DialogInterface dialog, String email, String password, String userName, String phoneNumber);
+
+        void dialogCancelClicked(DialogInterface dialog);
+    }
+
+    interface View {
+
+        void showSignInDialog();
+
+        void showRegisterDialog();
+
+        void hideDialog(DialogInterface dialog);
+
+        void showSnackBar(String message);
+
+        void showToast(String message);
+
+        void gotoMapActivity();
+    }
+
+    interface ModelCallbacks {
+
+        void onSignInSuccess();
+
+        void onRegisterSuccess(String driverId, String email, String password, String userName, String phoneNumber);
+
+        void onSignInFailure(String failureMessage);
+
+        void onRegisterFailure(String failureMessage);
+
+        void onSaveDriverSuccess(String userName);
+
+        void onSaveDriverFailure();
+    }
+
+    interface PresenterCallbacks {
+
+        void onSignInSuccess();
+
+        void onRegisterSuccess(String driverId, String email, String password, String userName, String phoneNumber);
+
+        void onSignInFailure(String failureMessage);
+
+        void onRegisterFailure(String failureMessage);
+
+        void onSaveDriverSuccess(String userName);
+
+        void onSaveDriverFailure();
+    }
+}
